3ae7a97a5b25671dbd4af8436152819ff614bfa8
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-08 12:09:24 +0530

    Visualize the Data

--- iris_classification.py
+++ iris_classification.py
@@ -1,4 +1,7 @@
 from sklearn.datasets import load_iris
+
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
 
 # Load the dataset
@@ -10,3 +13,9 @@ data['species'] = iris.target
 
 # Display the first five rows
 print(data.head())
+
+
+
+# Pairplot to visualize feature relationships
+sns.pairplot(data, hue='species', palette='Set2')
+plt.show()
