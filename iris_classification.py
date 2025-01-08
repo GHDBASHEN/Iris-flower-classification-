c596b6e56f29d065362ec01490fa241cb7223db5
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-08 12:26:24 +0530

    Train a Model

--- iris_classification.py
+++ iris_classification.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -27,3 +28,10 @@ y = iris.target  # Labels
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+
+
+# Train the model
+model = LogisticRegression(max_iter=200)
+model.fit(X_train, y_train)
+
