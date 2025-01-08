bf802a28bf815a42e5cb957cd02b5805d4f0888b
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-08 12:27:34 +0530

    Make Predictions and Evaluate

--- iris_classification.py
+++ iris_classification.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -34,4 +35,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 # Train the model
 model = LogisticRegression(max_iter=200)
 model.fit(X_train, y_train)
+
+
+
+
+# Predict on test data
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
 
