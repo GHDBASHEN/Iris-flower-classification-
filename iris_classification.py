0b3b1eaa49968419207f3c267b913e3f48c164a4
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-08 12:10:14 +0530

    Preprocess the Data

--- iris_classification.py
+++ iris_classification.py
@@ -1,4 +1,5 @@
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -14,8 +15,15 @@ data['species'] = iris.target
 # Display the first five rows
 print(data.head())
 
-
-
 # Pairplot to visualize feature relationships
 sns.pairplot(data, hue='species', palette='Set2')
 plt.show()
+
+
+
+# Split features and target
+X = iris.data  # Features
+y = iris.target  # Labels
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
